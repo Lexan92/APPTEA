1dd6febbdd753f3b455c8b20e58d11bc0dd7442c
Lexan92 <luis_grennby92@hotmail.com>
2020-05-16 22:24:09 -0600

    Se agregaron opciones del menu lateral de terapeuta y cuidador, tambien las respectivas vistas.

--- app/src/main/java/com/example/apptea/MainActivity.java
+++ app/src/main/java/com/example/apptea/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_gestion_habilidad,R.id.nav_gestion_juego,R.id.nav_gestion_pictograma,R.id.nav_registro_perfil)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);

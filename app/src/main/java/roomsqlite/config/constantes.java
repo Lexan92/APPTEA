bdc9d7b30fb1642d393e75a5ba9d789d3dfd52c4
Lexan92 <luis_grennby92@hotmail.com>
2020-05-18 18:13:33 -0600

    se agrego packages para el uso de las entidades,dao y database, se agrego configuracion de database

---
+++ app/src/main/java/roomsqlite/config/constantes.java
@@ -0,0 +1,10 @@
+package roomsqlite.config;
+
+public class constantes {
+    //NOMBRE DE LA BASE DE DATOS
+    private static String BD_NAME = "app_tea";
+
+    public static String getBdName() {
+        return BD_NAME;
+    }
+}

8cb80b382396854344227234eb42c7092309ee03
Oscar Turish <ortg_10@hotmail.com>
2020-05-19 22:07:14 -0600

    Update CatalogoHabCotidianas.java
    Se agrego campos de categoria habilidad cotidiana

--- app/src/main/java/roomsqlite/entidades/CatalogoHabCotidianas.java
+++ app/src/main/java/roomsqlite/entidades/CatalogoHabCotidianas.java
@@ -1,14 +1,16 @@
 package roomsqlite.entidades;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName= "cat_habilidad_cotidiana")
 public class CatalogoHabCotidianas {
+    @PrimaryKey(autoGenerate = true)
+    private int cat_hab_cotidiana_id;
 
-
-
-
+    @NonNull
+    private String cat_hab_cotidiana_nombre;
 
 
 }

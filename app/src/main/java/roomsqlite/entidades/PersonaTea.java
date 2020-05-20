c0ba258bde1f7827d6586da4396ca7d14142897f
elsyc <elsycampos2012@gmail.com>
2020-05-20 16:18:19 -0600

    entitys usuario y persona tea

--- app/src/main/java/roomsqlite/entidades/PersonaTea.java
+++ app/src/main/java/roomsqlite/entidades/PersonaTea.java
@@ -1,4 +1,72 @@
 package roomsqlite.entidades;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity(tableName = "persona_tea")
 public class PersonaTea {
+    @PrimaryKey(autoGenerate = true)
+    private int persona_id;
+    @NonNull
+    private String persona_nombre;
+    @NonNull
+    private String persona_apellido;
+    @NonNull
+    private Date persona_fecha_nac;
+    @NonNull
+    private char persona_sexo;
+
+    public PersonaTea(int persona_id, @NonNull String persona_nombre, @NonNull String persona_apellido, @NonNull Date persona_fecha_nac, char persona_sexo) {
+        this.persona_id = persona_id;
+        this.persona_nombre = persona_nombre;
+        this.persona_apellido = persona_apellido;
+        this.persona_fecha_nac = persona_fecha_nac;
+        this.persona_sexo = persona_sexo;
+    }
+
+    public int getPersona_id() {
+        return persona_id;
+    }
+
+    public void setPersona_id(int persona_id) {
+        this.persona_id = persona_id;
+    }
+
+    @NonNull
+    public String getPersona_nombre() {
+        return persona_nombre;
+    }
+
+    public void setPersona_nombre(@NonNull String persona_nombre) {
+        this.persona_nombre = persona_nombre;
+    }
+
+    @NonNull
+    public String getPersona_apellido() {
+        return persona_apellido;
+    }
+
+    public void setPersona_apellido(@NonNull String persona_apellido) {
+        this.persona_apellido = persona_apellido;
+    }
+
+    @NonNull
+    public Date getPersona_fecha_nac() {
+        return persona_fecha_nac;
+    }
+
+    public void setPersona_fecha_nac(@NonNull Date persona_fecha_nac) {
+        this.persona_fecha_nac = persona_fecha_nac;
+    }
+
+    public char getPersona_sexo() {
+        return persona_sexo;
+    }
+
+    public void setPersona_sexo(char persona_sexo) {
+        this.persona_sexo = persona_sexo;
+    }
 }
--- app/src/main/java/roomsqlite/entidades/Usuario.java
+++ app/src/main/java/roomsqlite/entidades/Usuario.java
@@ -1,4 +1,109 @@
 package roomsqlite.entidades;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName= "usuario")
 public class Usuario {
+    @PrimaryKey(autoGenerate = true)
+    private int usuario_id;
+
+    @NonNull
+    private String usuario_nombre;
+    @NonNull
+    private String usuario_apellido;
+    @NonNull
+    private String contrasenia;
+    @NonNull
+    private String correo;
+    @NonNull
+    private int telefono;
+    @NonNull
+    private  String direccion;
+    @NonNull
+    private int codigo_verificacion;
+
+    public Usuario(int usuario_id, @NonNull String usuario_nombre, @NonNull String usuario_apellido, @NonNull String contrasenia, @NonNull String correo, int telefono, @NonNull String direccion, int codigo_verificacion) {
+        this.usuario_id = usuario_id;
+        this.usuario_nombre = usuario_nombre;
+        this.usuario_apellido = usuario_apellido;
+        this.contrasenia = contrasenia;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.codigo_verificacion = codigo_verificacion;
+    }
+
+    public int getUsuario_id() {
+        return usuario_id;
+    }
+
+    public void setUsuario_id(int usuario_id) {
+        this.usuario_id = usuario_id;
+    }
+
+    @NonNull
+    public String getUsuario_nombre() {
+        return usuario_nombre;
+    }
+
+    public void setUsuario_nombre(@NonNull String usuario_nombre) {
+        this.usuario_nombre = usuario_nombre;
+    }
+
+    @NonNull
+    public String getUsuario_apellido() {
+        return usuario_apellido;
+    }
+
+    public void setUsuario_apellido(@NonNull String usuario_apellido) {
+        this.usuario_apellido = usuario_apellido;
+    }
+
+    @NonNull
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(@NonNull String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    @NonNull
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(@NonNull String correo) {
+        this.correo = correo;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    @NonNull
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(@NonNull String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getCodigo_verificacion() {
+        return codigo_verificacion;
+    }
+
+    public void setCodigo_verificacion(int codigo_verificacion) {
+        this.codigo_verificacion = codigo_verificacion;
+    }
+
+
 }

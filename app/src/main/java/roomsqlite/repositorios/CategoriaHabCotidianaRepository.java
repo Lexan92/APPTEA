ea6d69fc0465afad95cf534c5490f15133b0c566
Oscar Turish <ortg_10@hotmail.com>
2020-05-23 17:23:13 -0600

    Se creo repository y viewModel CatHabcotidianas

--- app/src/main/java/roomsqlite/repositorios/CategoriaHabCotidianaRepository.java
+++ app/src/main/java/roomsqlite/repositorios/CategoriaHabCotidianaRepository.java
@@ -10,5 +10,38 @@
 
 package roomsqlite.repositorios;
 
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import roomsqlite.dao.CategoriaHabCotidianaDao;
+import roomsqlite.database.appDatabase;
+import roomsqlite.entidades.CategoriaHabCotidiana;
+
 public class CategoriaHabCotidianaRepository {
+
+    private CategoriaHabCotidianaDao categoriaHabCotidianaDao;
+    private LiveData<List<CategoriaHabCotidiana>> categoriaHabCotidianaAll;
+
+
+    public CategoriaHabCotidianaRepository(Application application){
+        appDatabase db = appDatabase.getDatabase(application);
+        categoriaHabCotidianaDao = db.categoriaHabCotidianaDao();
+        categoriaHabCotidianaAll = categoriaHabCotidianaDao.getCatalogoHabCotidiana();
+    }
+
+    public LiveData<List<CategoriaHabCotidiana>> getTodasCategoriaHabCotidiana(){
+        return categoriaHabCotidianaAll;
+    }
+
+    public void insert(CategoriaHabCotidiana categoriaHabCotidiana){
+        categoriaHabCotidianaDao.insert(categoriaHabCotidiana);
+    }
+
+
+
+
+
 }

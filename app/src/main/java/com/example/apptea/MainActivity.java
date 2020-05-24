d71e617fb9d2d6de62440b0e8376ec5e802b7966
Oscar Turish <ortg_10@hotmail.com>
2020-05-24 16:29:36 -0600

    Actualizacion de fragmetn
    falta mostrar

--- app/src/main/java/com/example/apptea/MainActivity.java
+++ app/src/main/java/com/example/apptea/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/apptea/ui/categoriahabilidadcotidiana/CategoriaHabCotidianaFragment.java
+++ app/src/main/java/com/example/apptea/ui/categoriahabilidadcotidiana/CategoriaHabCotidianaFragment.java
@@ -8,27 +8,19 @@
  *
  */
 
-/*
- *
- * Nombre del Autor
- * 18/05/2020
- * Esta actividad hace el llamado a la lista de roles
- * /
- *
- *
- */
-
 package com.example.apptea.ui.categoriahabilidadcotidiana;
 
 import android.app.Application;
 import android.os.Bundle;
-import android.view.View;
 
-import androidx.annotation.NonNull;
-import androidx.lifecycle.AndroidViewModel;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.apptea.R;
 
@@ -37,23 +29,32 @@ import java.util.List;
 import roomsqlite.entidades.CategoriaHabCotidiana;
 import roomsqlite.repositorios.CategoriaHabCotidianaRepository;
 
-public class CategoriaHabCotidianaViewModel extends AndroidViewModel {
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CategoriaHabCotidianaFragment extends Fragment {
 
     private CategoriaHabCotidianaRepository categoriaHabCotidianaRepository;
     private LiveData<List<CategoriaHabCotidiana>> categoriaHabCotidianaAll;
 
-    protected void onCreate(Bundle savedInstanceState, RecyclerView container) {
-
-
-        RecyclerView recyclerView = container.findViewById(R.id.recyclerview_cat_hab_cotidiana);
-        final CategoriaHabCotidianaAdapter adapter = new CategoriaHabCotidianaAdapter(getApplication().getApplicationContext());
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getApplication().getApplicationContext()));
+    public CategoriaHabCotidianaFragment(){
 
     }
 
-    public CategoriaHabCotidianaViewModel(Application application){
-        super(application);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gestion_habilidad, container, false);
+
+
+
+
+    }
+
+/*
+    public CategoriaHabCotidianaFragment(Application application) {
+
         categoriaHabCotidianaRepository = new CategoriaHabCotidianaRepository(application);
         categoriaHabCotidianaAll = categoriaHabCotidianaRepository.getTodasCategoriaHabCotidiana();
     }
@@ -64,8 +65,7 @@ public class CategoriaHabCotidianaViewModel extends AndroidViewModel {
 
     public void insert(CategoriaHabCotidiana categoriaHabCotidiana){
         categoriaHabCotidianaRepository.insert(categoriaHabCotidiana);
-    }
-
+    }*/
 
 
 }

2f421241a5ec8d7d3282f9d368ea40494715954f
elsyc <elsycampos2012@gmail.com>
2020-05-30 20:12:27 -0600

    gestion tea

--- app/src/main/java/com/example/apptea/ui/menuPrincipal/MenuPrincipalFragment.java
+++ app/src/main/java/com/example/apptea/ui/menuPrincipal/MenuPrincipalFragment.java
@@ -81,6 +81,6 @@ public class MenuPrincipalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_principal, container, false);
+        return inflater.inflate(R.layout.fragment_gestion_persona_tea, container, false);
     }
 }

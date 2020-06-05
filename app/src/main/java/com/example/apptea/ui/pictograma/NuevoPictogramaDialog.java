e615892f592f070ee98d6e2c44996a9061f81e68
Lexan92 <luis_grennby92@hotmail.com>
2020-06-04 18:39:05 -0600

    Imagen Pictograma se agrego permisos, se construyo el layout para agregar pictogramas soportanto imagenes. Logica pendiente.

---
+++ app/src/main/java/com/example/apptea/ui/pictograma/NuevoPictogramaDialog.java
@@ -0,0 +1,20 @@
+
+
+
+
+package com.example.apptea.ui.pictograma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.apptea.R;
+
+public class NuevoPictogramaDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_pictograma_dialog);
+    }
+}

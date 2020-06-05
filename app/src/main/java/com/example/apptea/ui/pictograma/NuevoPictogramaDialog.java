e6426a2a679ab5954b0cb2c7a518a3aebe4ee58a
Lexan92 <luis_grennby92@hotmail.com>
2020-06-05 01:04:31 -0600

    Captura Imagen y Galeria la aplicacion ya permite acceder a la galeria por una foto y ademas ya permite realizar capturas desde la camara. Pendiente la logica del guardado de la imagen.

--- app/src/main/java/com/example/apptea/ui/pictograma/NuevoPictogramaDialog.java
+++ app/src/main/java/com/example/apptea/ui/pictograma/NuevoPictogramaDialog.java
@@ -4,17 +4,12 @@
 
 package com.example.apptea.ui.pictograma;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.ExifInterface;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Build;
@@ -28,12 +23,18 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
 import com.example.apptea.R;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.io.File;
+import java.io.IOException;
 
 import static android.Manifest.permission.CAMERA;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
@@ -44,57 +45,61 @@ public class NuevoPictogramaDialog extends AppCompatActivity {
     private static final String CARPETA_IMAGEN = "imagenes";//carpeta donde se guardan las fotos
     private static final String DIRECTORIO_IMAGEN = CARPETA_PRINCIPAL + CARPETA_IMAGEN;//ruta carpeta de directorios
     private String path;//almacena la ruta de la imagen
-    File fileImagen;
-    Bitmap bitmap;
-
-    private final int MIS_PERMISOS = 100;
     private static final int COD_SELECCIONA = 10;
     private static final int COD_FOTO = 20;
-
-    EditText nombrePictograma;
     MaterialButton botonRegistrar, botonSeleccionar;
     ImageView imgFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_MaterialComponents_Light_Dialog_MinWidth);
+
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_MaterialComponents_Light_Dialog_MinWidth);
         setContentView(R.layout.activity_nuevo_pictograma_dialog);
         imgFoto = findViewById(R.id.img_nuevo_pictograma);
         botonRegistrar = findViewById(R.id.button_guardar_imagen);
         botonSeleccionar = findViewById(R.id.button_seleccionar_imagen);
 
-        if (validaPermisos()){
+        if (validaPermisos()) {
             botonSeleccionar.setEnabled(true);
-        }else {
+        } else {
             botonSeleccionar.setEnabled(false);
         }
 
         botonSeleccionar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 cargarImagen();
+            }
+        });
+
+        botonRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Los duendes aun siguen trabajando con esta opcion :)", Toast.LENGTH_SHORT).show();
             }
         });
 
     }
 
 
-
     private boolean validaPermisos() {
-        if(Build.VERSION.SDK_INT<Build.VERSION_CODES.M){
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
             return true;
         }
 
-        if((checkSelfPermission(CAMERA)== PackageManager.PERMISSION_GRANTED)&&
-                (checkSelfPermission(WRITE_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED)){
+        if ((checkSelfPermission(CAMERA) == PackageManager.PERMISSION_GRANTED) &&
+                (checkSelfPermission(WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)) {
             return true;
         }
 
-        if((shouldShowRequestPermissionRationale(CAMERA)) ||
-                (shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE))){
+        if ((shouldShowRequestPermissionRationale(CAMERA)) ||
+                (shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE))) {
             cargarDialogoRecomendacion();
-        }else{
-            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE,CAMERA},100);
+        } else {
+            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, CAMERA}, 100);
         }
 
         return false;
@@ -102,7 +107,7 @@ public class NuevoPictogramaDialog extends AppCompatActivity {
 
     private void cargarDialogoRecomendacion() {
 
-        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(NuevoPictogramaDialog.this);
         builder.setTitle("Permisos Desactivados");
         builder.setMessage("Deberá aceptar los permisos para el correcto funcionamiento de la aplicación");
         builder.setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
@@ -116,7 +121,7 @@ public class NuevoPictogramaDialog extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE,CAMERA},100);
+                    requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, CAMERA}, 100);
                 }
             }
         });
@@ -127,11 +132,11 @@ public class NuevoPictogramaDialog extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if(requestCode==100){
-            if(grantResults.length==2 && grantResults[0]==PackageManager.PERMISSION_GRANTED
-                    && grantResults[1]==PackageManager.PERMISSION_GRANTED){
+        if (requestCode == 100) {
+            if (grantResults.length == 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
                 botonSeleccionar.setEnabled(true);
-            }else{
+            } else {
                 solicitarPermisosManual();
             }
         }
@@ -140,22 +145,22 @@ public class NuevoPictogramaDialog extends AppCompatActivity {
     private void solicitarPermisosManual() {
 
 
-        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(NuevoPictogramaDialog.this);
         builder.setTitle("¿Desea configurar los permisos de forma manual?");
         builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 //Toast.makeText(getApplicationContext(),"Los permisos no fueron aceptados",Toast.LENGTH_SHORT).show();
-                Snackbar.make(botonSeleccionar,"Los permisos no fueron aceptados",Snackbar.LENGTH_LONG);
+                Snackbar.make(botonSeleccionar, "Los permisos no fueron aceptados", Snackbar.LENGTH_LONG);
                 dialog.dismiss();
             }
         });
         builder.setPositiveButton("SI", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                Intent intent=new Intent();
+                Intent intent = new Intent();
                 intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                Uri uri=Uri.fromParts("package",getPackageName(),null);
+                Uri uri = Uri.fromParts("package", getPackageName(), null);
                 intent.setData(uri);
                 startActivity(intent);
             }
@@ -165,20 +170,22 @@ public class NuevoPictogramaDialog extends AppCompatActivity {
 
 
     private void cargarImagen() {
-        final CharSequence[] opciones={"Tomar Foto","Cargar Imagen","Cancelar"};
-        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+
+
+        final CharSequence[] opciones = {"Tomar Foto", "Cargar Imagen", "Cancelar"};
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(NuevoPictogramaDialog.this);
         builder.setTitle("Seleccione una opción");
         builder.setItems(opciones, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int i) {
-                if (opciones[i].equals("Tomar Foto")){
+                if (opciones[i].equals("Tomar Foto")) {
                     tomarFotografia();
-                }else{
-                    if (opciones[i].equals("Cargar Imagen")){
-                        Intent intent=new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                } else {
+                    if (opciones[i].equals("Cargar Imagen")) {
+                        Intent intent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                         intent.setType("image/");
-                        startActivityForResult(intent.createChooser(intent,"Seleccione la Aplicación"),COD_SELECCIONA);
-                    }else{
+                        startActivityForResult(intent.createChooser(intent, "Seleccione la Aplicación"), COD_SELECCIONA);
+                    } else {
                         dialog.dismiss();
                     }
                 }
@@ -186,42 +193,41 @@ public class NuevoPictogramaDialog extends AppCompatActivity {
 
             }
         });
+        builder.show();
 
 
     }
 
     private void tomarFotografia() {
-        File fileImagen=new File(Environment.getExternalStorageDirectory(),DIRECTORIO_IMAGEN);
+        File fileImagen = new File(Environment.getExternalStorageDirectory(), DIRECTORIO_IMAGEN);
 
-        boolean isCreada=fileImagen.exists();
-        String nombreImagen="";
-        if(isCreada==false){
-            isCreada=fileImagen.mkdirs();
+        boolean isCreada = fileImagen.exists();
+        String nombreImagen = "";
+        if (isCreada == false) {
+            isCreada = fileImagen.mkdirs();
         }
 
-        if(isCreada==true){
-            nombreImagen=(System.currentTimeMillis()/1000)+".jpg";
+        if (isCreada == true) {
+            nombreImagen = (System.currentTimeMillis() / 1000) + ".jpg";
         }
 
 
-        path=Environment.getExternalStorageDirectory()+
-                File.separator+DIRECTORIO_IMAGEN+File.separator+nombreImagen;
+        path = Environment.getExternalStorageDirectory() +
+                File.separator + DIRECTORIO_IMAGEN + File.separator + nombreImagen;
 
-        File imagen=new File(path);
+        File imagen = new File(path);
 
-        Intent intent=null;
-        intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Intent intent = null;
+        intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         ////
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.N)
-        {
-            String authorities=getApplicationContext().getPackageName()+".provider";
-            Uri imageUri= FileProvider.getUriForFile(this,authorities,imagen);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            String authorities = getApplicationContext().getPackageName() + ".provider";
+            Uri imageUri = FileProvider.getUriForFile(this, authorities, imagen);
             intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-        }else
-        {
+        } else {
             intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imagen));
         }
-        startActivityForResult(intent,COD_FOTO);
+        startActivityForResult(intent, COD_FOTO);
     }
 
 
@@ -229,11 +235,11 @@ public class NuevoPictogramaDialog extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode==RESULT_OK){
+        if (resultCode == RESULT_OK) {
 
-            switch (requestCode){
+            switch (requestCode) {
                 case COD_SELECCIONA:
-                    Uri miPath=data.getData();
+                    Uri miPath = data.getData();
                     imgFoto.setImageURI(miPath);
                     break;
 
@@ -242,11 +248,14 @@ public class NuevoPictogramaDialog extends AppCompatActivity {
                             new MediaScannerConnection.OnScanCompletedListener() {
                                 @Override
                                 public void onScanCompleted(String path, Uri uri) {
-                                    Log.i("Ruta de almacenamiento","Path: "+path);
+                                    Log.i("Ruta de almacenamiento", "Path: " + path);
                                 }
                             });
 
-                    Bitmap bitmap= BitmapFactory.decodeFile(path);
+                    Bitmap bitmap = BitmapFactory.decodeFile(path);
+                    //se llama al metodo para reorientar la imagen de manera correcta al presentarlo en
+                    //el imageview
+                    imgFoto.setRotation(obtenerOrientacionFoto(path));
                     imgFoto.setImageBitmap(bitmap);
 
                     break;
@@ -255,4 +264,32 @@ public class NuevoPictogramaDialog extends AppCompatActivity {
 
         }
     }
+
+    //metodo que asigna la orientacion correcta a la imagen
+    private int obtenerOrientacionFoto(String path) {
+        int rotate =0;
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,0);
+
+        switch (orientation){
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                rotate=180;
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                rotate=90;
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                rotate=90;
+            default:
+                rotate = 0;
+
+        }
+        //retorna el grado correcto de rotacion
+        return rotate;
     }
+}

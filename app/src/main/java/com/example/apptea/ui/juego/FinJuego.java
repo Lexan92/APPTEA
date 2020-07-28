2c55372d92335684ffbb084ad90b5ddba24ff58d
Lexan92 <luis_grennby92@hotmail.com>
2020-07-27 21:45:41 -0600

    Juego Se agrego TTS al momento de pulsar sobre las opciones en los juegos

--- app/src/main/java/com/example/apptea/ui/juego/FinJuego.java
+++ app/src/main/java/com/example/apptea/ui/juego/FinJuego.java
@@ -6,13 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.apptea.R;
+import com.example.apptea.utilidades.TTSManager;
 
 public class FinJuego extends AppCompatActivity {
 
+    TTSManager ttsManager = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fin_juego);
+        ttsManager = new TTSManager();
+        ttsManager.init(getApplicationContext());
+        ttsManager.initQueue("Fin del Juego, Bien hecho ");
+
     }
 
     public void listaJuegos(View view) {
